7995b8535c0def41b2cc3fa3e1f705e3d563efc1
shutstart <chirag.189301201@muj.manipal.edu>
2019-06-26 10:11:15 +0530

    added the dropzone directive

--- src/app/dropzone.directive.ts
+++ src/app/dropzone.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, HostListener, Output, EventEmitter } from '@angular/core';
 
 @Directive({
-  selector: '[appDropzone]'
+  selector: '[dropZone]'
 })
 export class DropzoneDirective {
   @Output() dropped = new EventEmitter<FileList>();
